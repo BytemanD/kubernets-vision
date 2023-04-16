f8df54b69e28bae8aa6bcb6453bb0db5af0d8567
fjboy <fjboy.z@outlook.com>
2023-04-16 17:56:55 +0800

    支持通过yaml创建workflow
    Signed-off-by: fjboy <fjboy.z@outlook.com>

--- webapp/src/components/dashboard/WorkloadPage.vue
+++ webapp/src/components/dashboard/WorkloadPage.vue
@@ -3,10 +3,6 @@
         <v-btn-toggle mandatory dense rounded active-class='primary white--text'  class="mb-4 mt-0" v-model="toggleWorkloadIndex">
             <v-btn v-for="(item, i) in workLoadRouter" v-bind:key="item.name"
                 @click="refreshRouter(i)">{{ i18n.t(item.name) }}</v-btn>
-            <!-- <v-btn @click="routeTo('/workload/daemonset')">{{ i18n.t('daemonset') }}</v-btn>
-            <v-btn @click="routeTo('/workload/statefulset')">{{i18n.t('statefulset')}}</v-btn>
-            <v-btn @click="routeTo('/workload/cronjob')">{{i18n.t('cronjob')}}</v-btn>
-            <v-btn @click="routeTo('/workload/job')">{{i18n.t('job')}}</v-btn> -->
         </v-btn-toggle>
         <router-view />
     </div>
